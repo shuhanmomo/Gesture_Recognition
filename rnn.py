ede4b961d56025d12500f540c15e0dd87a4b1dc2
Tracy Miao <tracymiao98@gmail.com>
2025-03-07 23:54:41 -0500

    finish RNN

--- rnn.py
+++ rnn.py
@@ -1,10 +1,11 @@
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, LSTM, Dense, concatenate
+from keras.layers import Input, LSTM, Dense, Bidirectional, Concatenate
 
 from normalize_frames import normalize_frames
 from load_gestures import load_gestures
 
+USE_BIDIRECTION = True
 # 9. FORMAT DATA
 gesture_sets = load_gestures()
 gesture_sets = normalize_frames(gesture_sets, 36)
@@ -33,16 +34,25 @@ latent_dim = 16
 
 input_layer = Input(shape=(X.shape[1:]))
 lstm = LSTM(latent_dim)(input_layer)
-dense = Dense(latent_dim, activation='relu')(lstm)
-pred = Dense(len(gesture_sets), activation='softmax')(dense)
+
+dense = Dense(latent_dim, activation="relu")(lstm)
+if USE_BIDIRECTION:
+    lstm_backward = LSTM(latent_dim, go_backwards=True)(input_layer)
+    dense_backward = Dense(latent_dim, activation="relu")(lstm_backward)
+    merged = Concatenate()([dense, dense_backward])
+    pred = Dense(len(gesture_sets), activation="softmax")(merged)
+else:
+    pred = Dense(len(gesture_sets), activation="softmax")(dense)
 
 model = Model(inputs=input_layer, outputs=pred)
-model.compile(loss="sparse_categorical_crossentropy", optimizer='adam', metrics=["acc"])
+model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["acc"])
 
-model.fit(X,
-          Y,
-          epochs=epochs,
-          batch_size=batch_size,
-          verbose=1,
-          validation_split=0.3,
-          shuffle=True)
+model.fit(
+    X,
+    Y,
+    epochs=epochs,
+    batch_size=batch_size,
+    verbose=1,
+    validation_split=0.3,
+    shuffle=True,
+)
