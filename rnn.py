57d25b74011d2db8d58aa5629b32a979d89bddde
Tracy Miao <tracymiao98@gmail.com>
2025-03-09 18:28:24 -0400

    gru vs lstm

--- rnn.py
+++ rnn.py
@@ -1,11 +1,12 @@
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, LSTM, Dense, Bidirectional, Concatenate
+from keras.layers import Input, LSTM, Dense, Bidirectional, Concatenate, GRU
 
 from normalize_frames import normalize_frames
 from load_gestures import load_gestures
 
 USE_BIDIRECTION = True
+USE_GRU = False
 # 9. FORMAT DATA
 gesture_sets = load_gestures()
 gesture_sets = normalize_frames(gesture_sets, 36)
@@ -32,27 +33,45 @@ batch_size = 12
 epochs = 100
 latent_dim = 16
 
-input_layer = Input(shape=(X.shape[1:]))
-lstm = LSTM(latent_dim)(input_layer)
 
-dense = Dense(latent_dim, activation="relu")(lstm)
-if USE_BIDIRECTION:
-    lstm_backward = LSTM(latent_dim, go_backwards=True)(input_layer)
-    dense_backward = Dense(latent_dim, activation="relu")(lstm_backward)
-    merged = Concatenate()([dense, dense_backward])
-    pred = Dense(len(gesture_sets), activation="softmax")(merged)
-else:
-    pred = Dense(len(gesture_sets), activation="softmax")(dense)
+def train_rnn(use_gru=False, use_bidirectional=True):
 
-model = Model(inputs=input_layer, outputs=pred)
-model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["acc"])
+    input_layer = Input(shape=(X.shape[1:]))
 
-model.fit(
-    X,
-    Y,
-    epochs=epochs,
-    batch_size=batch_size,
-    verbose=1,
-    validation_split=0.3,
-    shuffle=True,
-)
+    if use_gru:
+        rnn = GRU(latent_dim)(input_layer)
+    else:
+        rnn = LSTM(latent_dim)(input_layer)
+
+    dense = Dense(latent_dim, activation="relu")(rnn)
+    if use_bidirectional:
+        if use_gru:
+            rnn_backward = GRU(latent_dim, go_backwards=True)(input_layer)
+        else:
+            rnn_backward = LSTM(latent_dim, go_backwards=True)(input_layer)
+        dense_backward = Dense(latent_dim, activation="relu")(rnn_backward)
+        merged = Concatenate()([dense, dense_backward])
+        pred = Dense(len(gesture_sets), activation="softmax")(merged)
+    else:
+        pred = Dense(len(gesture_sets), activation="softmax")(dense)
+
+    model = Model(inputs=input_layer, outputs=pred)
+    model.compile(
+        loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["acc"]
+    )
+
+    history = model.fit(
+        X,
+        Y,
+        epochs=epochs,
+        batch_size=batch_size,
+        verbose=1,
+        validation_split=0.3,
+        shuffle=True,
+    )
+    val_acc = max(history.history["val_acc"])
+
+    return val_acc, history.history
+
+
+train_rnn()
